6ce764dcee1e910b96e9ff384b47905f91fb1096
Designnify <designnify@gmail.com>
2016-07-31 05:11:15 +0300

    Uplife Starts
    New theme for Uplife

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+//* Start the engine
+include_once( get_template_directory() . '/lib/init.php' );
+
+//* Child theme (do not remove)
+define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+define( 'CHILD_THEME_VERSION', '2.1.2' );
+
+//* Enqueue Styles and Scripts
+add_action( 'wp_enqueue_scripts', 'genesis_sample_scripts' );
+function genesis_sample_scripts() {
+
+	//* Add Google Fonts
+	wp_register_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'google-fonts' );
+
+	//* Add compiled JS
+	wp_enqueue_script( 'genesis-sample-scripts', get_stylesheet_directory_uri() . '/js/project' . $minnified . '.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
+
+}
+
+//* Add HTML5 markup structure
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
+
+//* Add viewport meta tag for mobile browsers
+add_theme_support( 'genesis-responsive-viewport' );
+
+//* Add support for custom background
+add_theme_support( 'custom-background' );
+
+//* Add support for 3-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 3 );
