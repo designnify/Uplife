33c949e17ee5a374abad930e10523558dadadb9b
Designnify <designnify@gmail.com>
2016-08-07 17:38:28 +0300

    Home Page & Blog Page customization

--- functions.php
+++ functions.php
@@ -35,8 +35,8 @@ add_theme_support( 'genesis-responsive-viewport' );
 //* Enable shortcodes in widgets
 add_filter('widget_text', 'do_shortcode');
 
-//* Add support for 1-column footer widgets
-add_theme_support( 'genesis-footer-widgets', 1 );
+//* Add support for 4-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 4 );
 
 //* Customize the credits
 add_filter( 'genesis_footer_creds_text', 'sp_footer_creds_text' );
@@ -47,6 +47,36 @@ function sp_footer_creds_text() {
 	echo ' &middot; Uplife &middot; All rights reserved &middot; Built by <a href="http://designnify.com/" target="blank">Designnify - Mauricio Alvarez</a>';
 	echo '</p></div>';
 }
+
+//* Add new tumbnail image sizes for post excerpts
+add_image_size( 'homepage-thumbnail', 280, 170, TRUE );
+add_image_size( 'homepage-blog-thumbnail', 280, 280, TRUE );
+add_image_size( 'post-featured-image-large', 700, 400, TRUE );
+
+//* Remove the entry meta in the entry header and footer
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
+remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
+
+//* Add Read More Link to Excerpts
+add_filter('excerpt_more', 'get_read_more_link');
+add_filter( 'the_content_more_link', 'get_read_more_link' );
+function get_read_more_link() {
+   return '...&nbsp;<p class="read-more-wrap"><a href="' . get_permalink() . '" class="read-more">[Read More]</a></p>';
+}
+
+//* Display Featured Image on top of the post
+add_action( 'genesis_entry_content', 'featured_post_image', 8 );
+function featured_post_image() {
+  if ( ! is_singular( 'post' ) )  return;
+	the_post_thumbnail('post-image');
+}
+
+//* Add previous and next post links after entry
+add_action( 'genesis_entry_footer', 'genesis_prev_next_post_nav' );
 
 //* Register widget areas 
 genesis_register_sidebar( array(
